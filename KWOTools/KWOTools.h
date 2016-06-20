0101c2c77184b2f68b6fe99cccfea78f41985da1
Kevin Wolkober <kevin.wolkober@gmail.com>
2016-06-20 17:04:07 +0800

    Greenfield

---
+++ KWOTools/KWOTools.h
@@ -0,0 +1,19 @@
+//
+//  KWOTools.h
+//  KWOTools
+//
+//  Created by Kevin Wolkober on 6/20/16.
+//  Copyright © 2016 Kevin Wolkober. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KWOTools.
+FOUNDATION_EXPORT double KWOToolsVersionNumber;
+
+//! Project version string for KWOTools.
+FOUNDATION_EXPORT const unsigned char KWOToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KWOTools/PublicHeader.h>
+
+
