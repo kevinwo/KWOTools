f76a1c653ac48f98a8c68ba40f319dbf26d7695f
Kevin Wolkober <kevin.wolkober@gmail.com>
2016-10-30 12:40:31 +0800

    Add date format/string convenience interface

---
+++ KWOTools/Foundation/Classes/Dater.swift
@@ -0,0 +1,26 @@
+//
+//  Date.swift
+//  KWOTools
+//
+//  Created by Kevin Wolkober on 10/30/16.
+//  Copyright © 2016 Kevin Wolkober. All rights reserved.
+//
+
+import Foundation
+
+public class Dater {
+
+    public var rawDate: Date
+    fileprivate var formatter = DateFormatter()
+
+    public init(date: Date, timeZone: TimeZone = TimeZone(secondsFromGMT: 0)!) {
+        self.rawDate = date
+        self.formatter.timeZone = timeZone
+    }
+
+    public func string(for format: String) -> String {
+        self.formatter.dateFormat = format
+
+        return formatter.string(from: self.rawDate)
+    }
+}
