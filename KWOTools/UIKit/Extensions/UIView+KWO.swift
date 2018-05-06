f8154ad7190355985640e746723b4c4c429c9ff1
Kevin Wolkober <kevin.wolkober@gmail.com>
2018-05-06 12:46:02 +0800

    Add convenience ignore invert colors

---
+++ KWOTools/UIKit/Extensions/UIView+KWO.swift
@@ -0,0 +1,16 @@
+//
+//  UIView+KWO.swift
+//  KWOTools
+//
+//  Created by Kevin Wolkober on 2/21/18.
+//
+
+import UIKit
+
+extension UIView {
+    public func setIgnoresInvertColors(_ shouldIgnore: Bool) {
+        if #available(iOS 11.0, *) {
+            self.accessibilityIgnoresInvertColors = shouldIgnore
+        }
+    }
+}
