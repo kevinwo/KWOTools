024e9ee783ca0685a662d9281724a78c1db2fecf
Kevin Wolkober <kevin.wolkober@gmail.com>
2018-05-06 12:43:38 +0800

    Add alert controller convenience methods

---
+++ KWOTools/UIKit/Extensions/UIAlertController+KWO.swift
@@ -0,0 +1,50 @@
+//
+//  UIAlertController+KWO.swift
+//  KWOTools
+//
+//  Created by Kevin Wolkober on 1/1/18.
+//
+
+import UIKit.UIViewController
+import UIKit.UIAlertController
+
+public typealias KWOAlertHandlerBlock = () -> Void
+
+extension UIAlertController {
+    public static func kwo_errorAlert(_ error: NSError) -> UIAlertController {
+        let controller = UIAlertController(title: error.localizedFailureReason, message: error.localizedRecoverySuggestion ?? error.localizedDescription, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK".kwo_localized, style: .default, handler: nil)
+        controller.addAction(action)
+
+        return controller
+    }
+
+    public static func kwo_alert(
+        title: String,
+        message: String? = nil,
+        confirmationTitle: String? = nil,
+        cancelTitle: String? = nil,
+        isDestructive: Bool = false,
+        confirmationHandler: (() -> Void)? = nil,
+        cancelHandler: (() -> Void)? = nil) -> UIAlertController {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+        if let title = confirmationTitle {
+            let style: UIAlertActionStyle = isDestructive ? .destructive : .default
+            alertVC.addAction(UIAlertAction(title: title, style: style, handler: { (action) -> Void in
+                confirmationHandler?()
+            }))
+        }
+
+        let title = cancelTitle ?? "OK".kwo_localized
+        alertVC.addAction(UIAlertAction(title: title, style: .cancel, handler: { (action) -> Void in
+            cancelHandler?()
+        }))
+
+        return alertVC
+    }
+
+    public func kwo_show(in controller: UIViewController) {
+        controller.present(self, animated: true, completion: nil)
+    }
+}
